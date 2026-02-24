c0c34ee9dd7b6fd0d3a66f025d80659ec191546c
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-02-24 18:39:50 +0400

    fix vite base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/silk4me/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
